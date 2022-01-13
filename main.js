38ebf43af7f832a78378b0777ec9a97fc4bd3f2c
Your Name <jtellezvelasco@gmail.com>
2022-01-12 19:20:01 -0600

    UI al ejercicio

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ function random(min, max) {
   }
 
 
-  function computerPlay() {
+function computerPlay() {
     let numj = random(1, 3)
         if (numj == 1) {
             return "Rock";
@@ -20,11 +20,14 @@ function random(min, max) {
 
 }
 
+let scorePlayer = 0
+let scoreComputer = 0
+let round = 0
 
 function playRound(playerSelection, computerSelection) {
     let a = playerSelection.toLowerCase();
     let b = computerSelection.toLowerCase();
-    let score = 0
+    round += 1
 
     if (a == b) {
         return "Tie!"
@@ -54,28 +57,82 @@ function playRound(playerSelection, computerSelection) {
         return "You Win! Scissors beats Paper";
     }
   }
-  
-scorePlayer = 0
-scoreComputer = 0
 
 function game() {
-    for(let i = 1; i < 6; i++) {
-    console.log(i);
     const playerSelection = prompt("Rock, Paper, or Scissors?");
     const computerSelection = computerPlay();
     alert(playRound(playerSelection, computerSelection));
     console.log(playRound(playerSelection, computerSelection));
+
+    if (scoreComputer > scorePlayer){
+        console.log ("Player Score = " + scorePlayer + ". Computer Score = "+ scoreComputer+ ". You Lose!");
+    }
+    else if (scoreComputer < scorePlayer){
+        console.log ("Player Score = " + scorePlayer + ". Computer Score = "+ scoreComputer+ ". You Win!");
+    }
+    else if (scoreComputer = scorePlayer){
+        console.log ("Player Score = " + scorePlayer + ". Computer Score = "+ scoreComputer+ ". Tie!");
+    }
 }
 
-if (scoreComputer > scorePlayer){
-    console.log ("Player Score = " + scorePlayer + ". Computer Score = "+ scoreComputer+ ". You Lose!");
-}
-else if (scoreComputer < scorePlayer){
-    console.log ("Player Score = " + scorePlayer + ". Computer Score = "+ scoreComputer+ ". You Win!");
-}
-else if (scoreComputer = scorePlayer){
-    console.log ("Player Score = " + scorePlayer + ". Computer Score = "+ scoreComputer+ ". Tie!");
-}
-}
 
-game()
+const buttons = document.querySelectorAll("button");
+
+function logText(e) {
+    return this.id;
+    }
+
+
+buttons.forEach(button => button.addEventListener('click', () => {
+
+    if (round > 4 ) {
+        if (scoreComputer > scorePlayer){
+            const fin = document.createElement('div');
+            fin.classList.add('fin');
+            fin.textContent = "Player Score = " + scorePlayer + ". Computer Score = "+ scoreComputer+ ". You Lose! Refresh page."
+            container.appendChild(fin);
+        }
+        else if (scoreComputer < scorePlayer){
+            const fin = document.createElement('div');
+            fin.classList.add('fin');
+            fin.textContent = "Player Score = " + scorePlayer + ". Computer Score = "+ scoreComputer+ ". You Win! Refresh page."
+            container.appendChild(fin);
+ 
+        }
+        else if (scoreComputer = scorePlayer){
+            const fin = document.createElement('div');
+            fin.classList.add('fin');
+            fin.textContent = "Player Score = " + scorePlayer + ". Computer Score = "+ scoreComputer+ ". Tie! Refresh page."
+            container.appendChild(fin);
+        }
+    }
+
+    else {
+
+    let compu = computerPlay();
+    juego = playRound(button.id,compu);
+    message1 = button.id + " v " + compu;
+    message2 = "Round: " + round + " && Computer Score: " + scoreComputer + " && Player Score: " + scorePlayer;
+   
+    const scoreTable = document.createElement('div');
+    scoreTable.classList.add('scoreTable');
+    scoreTable.textContent = message1
+    container.appendChild(scoreTable);
+
+    const res = document.createElement('div');
+    res.classList.add('res');
+    res.textContent = juego
+    container.appendChild(res);
+
+
+    const scoreTable2 = document.createElement('div');
+    scoreTable2.classList.add('scoreTable2');
+    scoreTable2.textContent = message2;
+    container.appendChild(scoreTable2);
+
+    const lin= document.createElement('div');
+    lin.classList.add('lin');
+    lin.textContent = "---------------------------------------------------";
+    container.appendChild(lin);
+    }}))
+
